50253c031265819b8b3e5e104c039cee818f3808
Friedrich Clausen <friedrich.clausen@blackboard.com>
2019-06-04 14:44:10 +1000

    Allow skipping of missing dependencies in sub-package and also specify which list to copy to/from

--- bin/elm-deps-sync-cli.js
+++ bin/elm-deps-sync-cli.js
@@ -13,6 +13,9 @@ program
     .option('-d, --dry', 'Only print the changes to happen')
     .option('-q, --quiet', 'Will only print the final statement')
     .option('--note', 'Add a `test-dependencies` field to the second file')
+    .option('--from <from>', 'Which type of dependencies to sync from: "devDependencies" or "dependencies". Default: "dependencies"')
+    .option('--to <to>', 'Which type of dependencies to sync to: "devDependencies" or "dependencies". Default: "dependencies"')
+    .option('--skip-missing', 'Only change dependencies present in sub _and_ parent')
     .parse(process.argv);
 
 var files = program.args.slice(0, 2);
@@ -27,7 +30,7 @@ if (files.length === 0) {
 }
 
 if (files.length === 2) {
-    syncVersions(files[ 0 ], files[ 1 ], program.quiet, program.dry, program.note);
+    syncVersions(files[ 0 ], files[ 1 ], program.quiet, program.dry, program.note, program.skipMissing, program.from, program.to);
 } else {
     console.log(chalk.yellow('Please specify <parent> file and <sub> file to sync dependencies.'));
     process.exit(1);
